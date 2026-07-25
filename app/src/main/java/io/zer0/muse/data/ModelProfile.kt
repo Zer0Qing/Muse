2babb8e6089f81436a0101615b4ef907326b61b5
Zer0 <2119280571@qq.com>
2026-07-25 22:23:20 +0800

    v1.0.22

--- app/src/main/java/io/zer0/muse/data/ModelProfile.kt
+++
@@ -1,9 +0,0 @@
-package io.zer0.muse.data
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class ModelProfile(
-    val avatarUrl: String = "",
-    val showAvatar: Boolean = false,
-)
